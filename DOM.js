deec333990af9955970a5ce975f915dcdf427953
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-03-20 19:25:40 +0800

    v2.0:renew the RWD

--- DOM.js
+++ DOM.js
@@ -1,3 +1,25 @@
+let ul = $('#navUl')
+
 $('#mobileNav').click(function () {
-    $('#navUl').slideToggle()
+    ul.slideToggle()
+})
+
+$(document).ready(function () {
+    let $window = $(window)
+
+    function changeNav() {
+        if ($window.width()> 660) {
+            ul.css({
+                'display': 'flex',
+                'flex-direction': 'row'
+            })
+        } else {
+            ul.css({
+                'display': 'none',
+                'flex-direction': 'column' 
+            })
+        }
+    }
+
+    $window.resize(changeNav)
 })
