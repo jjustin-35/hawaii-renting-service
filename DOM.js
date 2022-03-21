44eaa85ee8cea8cf5ab328bf316d9202c0f771e9
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-03-21 15:22:03 +0800

    v3.02: add nav DOM

--- DOM.js
+++ DOM.js
@@ -1,12 +1,13 @@
 let ul = $('#navUl')
 
+// 手機視窗，漢堡選單
 $('#mobileNav').click(function () {
     ul.slideToggle()
 })
 
 $(document).ready(function () {
     let $window = $(window)
-
+// 視窗改變時，nav會自動調整
     function changeNav() {
         if ($window.width()> 660) {
             ul.css({
@@ -20,6 +21,23 @@ $(document).ready(function () {
             })
         }
     }
-
+    // 視窗改變時執行
     $window.resize(changeNav)
+// 滾動時，nav隱藏，滑鼠碰觸上方時彈出
+    let $nav = $('#nav')
+    let $hideNav = $('#hideNav')
+
+    $window.on('scroll', function () {
+        $nav.slideUp()
+        $hideNav.show()
+        if ($window.scrollTop() == 0) {
+            $nav.slideDown()
+            $hideNav.hide()
+        }
+    })
+    
+    $hideNav.mouseenter(function () {
+        $nav.slideDown()
+        // $nav.slideUp()
+    })
 })
